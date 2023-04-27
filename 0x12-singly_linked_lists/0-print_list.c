bb0967c9c2d545176f4ba88327f27cac6439d3fb
Salwa-BH <batahsalwa@gmail.com>
2023-04-27 17:12:38 +0100

    why tasks fail

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * size_t print_list(const list_t *h)
+ * print_list - check the code for Holberton School students.
  * @h: name of the list
- * Return: the number of nodes
+ * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
 {
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,11 +9,11 @@
  *
  * Description: singly linked list
  */
-typedef struct node
+typedef struct list_s
 {
-    char *str;
-    int len;
-    struct node *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
