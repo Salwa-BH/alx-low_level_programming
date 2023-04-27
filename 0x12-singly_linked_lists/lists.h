e19b6a6bc4ed1009e72b6bef2e347bf8831c8d6e
Salwa-BH <batahsalwa@gmail.com>
2023-04-27 16:00:29 +0100

    correct print_list betty 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,18 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 /**
- * struct list_t: single linked list
- * @str: string
- * @len: len of string
- * @next: points to next node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: singly linked list
+ * Description: singly linked list node structure
  */
-typedef struct node {
-    char *str;
-    int len;
-    struct node *next;
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
