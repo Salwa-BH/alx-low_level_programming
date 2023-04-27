dff7c6dc2b0593766232cd2a4237e3252194a930
Salwa-BH <batahsalwa@gmail.com>
2023-04-27 15:49:22 +0100

    correct print_list3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,23 @@
  * size_t print_list(const list_t *h) 
  * @h: name of the list
  * Return: the number of nodes
-*/
+ */
 size_t print_list(const list_t *h)
 {
-    size_t nbr = 0;
-    if( h == NULL)
-        return 0;
-    
+	int count = 0;
 
-    while(h != NULL)
-    {
-        nbr++;
-        if(h->str == NULL)
-            printf("[0] (nil)");
-        else
-            printf("[%d] %s",h->len, h->str);
-        h = h->next;
-    }
-    return nbr;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
