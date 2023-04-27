cdf157ea85efec3444b301fbedd5a52444d25e02
Salwa-BH <batahsalwa@gmail.com>
2023-04-27 09:47:08 -0700

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,19 +4,18 @@
 #include "lists.h"
 
 /**
- * void free_list(list_t *head)
- * Description: frees a list
+ * free_list - frees a list
  * @head: head of the list
  * Return: nothing
  */
 void free_list(list_t *head)
 {
-    if (head == NULL)
-        return;
-    while (head)
-    {
-        free_list(head->next);
-        free(head->str);
-        free(head);
-    }
+	if (head == NULL)
+		return;
+	while (head)
+	{
+	free_list(head->next);
+	free(head->str);
+	free(head);
+	}
 }
