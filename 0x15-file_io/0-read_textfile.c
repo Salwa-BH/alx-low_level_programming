cc88fab9f0e58b750b1ea57391dba3201d6d9ec7
Salwa-BH <batahsalwa@gmail.com>
2023-05-08 18:44:39 +0100

    correct betty in main.h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,16 @@
-/*
- * File: 0-read_textfile.c
- * Auth: Salwa BATAH.
- */
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file.
+ * @letters: the number of letters it should read and print
+ * Return:  the actual number of letters it could read and print
+ * or 0 if the function fails or filename is null
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	ssize_t open_value, read_value, write_value;
 	char *buffer;
 
 	if (filename == NULL)
@@ -26,18 +20,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	open_value = open(filename, O_RDONLY);
+	read_value = read(open_value, buffer, letters);
+	write_value = write(STDOUT_FILENO, buffer, read_value);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (open_value == -1 || read_value == -1 || write_value == -1
+	|| write_value != read_value)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(o);
+	close(open_value);
 
-	return (w);
+	return (write_value);
 }
